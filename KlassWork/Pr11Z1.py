6d72c7edc3b53b1e1b50f64be2673911fc9193bc
Nikita Dolgolenko <nikitozz29@yandex.ru>
2024-01-09 11:00:08 +0300

    раскидал по папкам

--- KlassWork/Pr11Z1.py
+++ KlassWork/Pr11Z1.py
@@ -3,34 +3,39 @@
 # цифр в этом числе, посчитать их сумму и среднее арифметическое. Определить количество нулей в этом числе.
 # Общение с пользователем организовать через меню.
 
-
-while  True:
-
+while True:
     num = int(input('Введите число > '))
+    print('Выберите номер\nМеню:')
+    print('1. Определить количество цифр')
+    print('2. Посчитать сумму цифр в числе')
+    print('3. Посчитать среднее арифметическое цифр в числе')
+    print('4. Определить количество нулей в числе')
+    print('5. Выход')
+    for i in range(0, 20):
+        print('=', end='')
+    
 
-    action = input('1.Определить количество цифр\n2.Посчитать сумму цифр\n3.Посчитать среднее арифметическое\n4.Определить количество нулей\n')
+    action = input('\n>>>')
 
-    if action == '1':
-        count = 0
-        str_num = str(num)
-        for i in str_num:
-            count += 1
-        print(f'Количество цифр {count}')
+    sum = 0
+    count = 0
+    zero_counter = 0
+
+    if action == '5':
+        break
+    elif action == '1':
+        print(f'В числе {len(str(num))} цифр')
     elif action == '2':
-        sum = 0
         for i in str(num):
             sum += int(i)
         print(f'Сумма цифр {sum}')
     elif action == '3':
-        count = 0
-        sum = 0
         for i in str(num):
-            count += 1
             sum += int(i)
-        print(f'Среднее арифметическое {sum/count}')
+            count += 1
+        print(f'Среднее арифметическое цифр в числе {sum/count}')
     elif action == '4':
-        count = 0
         for i in str(num):
             if i == '0':
-                count += 1
-        print(f'Количество нулей {count}')
+                zero_counter += 1
+        print(f'Количество нулей в числе {zero_counter}')
